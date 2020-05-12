ee5f9a138fdced3205d33e557abef09c48bbdcaf
Asthonishia <korx.klesk@gmail.com>
2020-05-12 10:56:25 +0200

    French translation

--- src/lang/index.ts
+++ src/lang/index.ts
@@ -2,16 +2,18 @@ import VueI18n from 'vue-i18n'
 
 import enLocale from 'element-ui/lib/locale/lang/en'
 import zhLocale from 'element-ui/lib/locale/lang/zh-CN'
+import frLocale from 'element-ui/lib/locale/lang/fr'
 
 import { formati18n } from '@/utils/i18n'
 
 const i18nModules: i18nLocaleModel = ['connections', 'settings', 'common', 'about']
 
-const { en, zh }: VueI18n.LocaleMessages = formati18n(i18nModules)
+const { en, zh, fr }: VueI18n.LocaleMessages = formati18n(i18nModules)
 
 const lang: VueI18n.LocaleMessages = {
   en: { ...en, ...enLocale },
   zh: { ...zh, ...zhLocale },
+  fr: { ...fr, ...frLocale },
 }
 
 export default lang
