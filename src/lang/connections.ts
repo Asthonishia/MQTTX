f524793f56a93c3ee086aee2fefffd2ef136b2bc
Asthonishia <korx.klesk@gmail.com>
2020-05-12 10:45:51 +0200

    French translation

--- src/lang/connections.ts
+++ src/lang/connections.ts
@@ -2,173 +2,216 @@ export default {
   connections: {
     zh: '连接',
     en: 'Connections',
+    en: 'Connections',
   },
   newConnections: {
     zh: '新建连接',
     en: 'New Connection',
+    fr: 'Nouvelle Connection',
   },
   search: {
     zh: '搜索',
     en: 'Search',
+    fr: 'Recherche',
   },
   searchByTopic: {
     zh: '按主题搜索',
     en: 'Search by Topic',
+    fr: 'Recherche par Topic',
   },
   topicCopied: {
     zh: '复制成功!',
     en: 'Topic copied!',
+    fr: 'Topic copié',
   },
   clearHistory: {
     zh: '清除历史记录',
     en: 'Clear Histroy',
+    fr: 'Vider l\'historique',
   },
   notConnect: {
     zh: '客户端未连接',
     en: 'Client not connected',
+    fr: 'Client non connecté',
   },
   disconnect: {
     zh: '断开连接',
     en: 'Disconnect',
+    fr: 'Déconnexion',
   },
   disconnected: {
     zh: '已断开连接',
     en: 'Disconnected',
+    fr: 'Déconnecté',
   },
   deleteConnect: {
     zh: '删除连接',
     en: 'Delete Connection',
+    fr: 'Supprimer la connection',
   },
   all: {
     zh: '全部',
     en: 'All',
+    fr: 'Tout',
   },
   received: {
     zh: '已接收',
     en: 'Received',
+    fr: 'Reçu',
   },
   published: {
     zh: '已发送',
     en: 'Published',
+    fr: 'Publié',
   },
   writeMsg: {
     zh: '请输入消息',
     en: 'Write a message',
+    fr: 'Écrire un message',
   },
   subscriptions: {
     zh: '订阅列表',
     en: 'Subscriptions',
+    fr: 'Souscriptions',
   },
   subscription: {
     zh: '订阅',
     en: 'subscription',
+    fr: 'Souscription',
   },
   newSubscription: {
     zh: '添加订阅',
     en: 'New Subscription',
+    fr: 'Nouvelle Souscription',
   },
   subFailed: {
     zh: '订阅失败',
     en: 'Subscribe Failed',
+    fr: 'Échec de la souscription',
   },
   connected: {
     zh: '已连接',
     en: 'Connected',
+    fr: 'Connecté',
   },
   connectFailed: {
     zh: '连接失败',
     en: 'Connect Failed',
+    fr: 'Échec de la connexion',
   },
   reconnect: {
     zh: '正在重连',
     en: 'Reconnecting',
+    fr: 'Reconnexion',
   },
   connectBtn: {
     zh: '连 接',
     en: 'Connect',
+    fr: 'Connexion',
   },
   disconnectedBtn: {
     zh: '断开连接',
     en: 'Disconnect',
+    fr: 'Déconnexion',
   },
   connectionExists: {
     zh: '连接数据已存在',
     en: 'Connection already exists',
+    fr: 'Connexion déjà existante',
   },
   brokerIP: {
     zh: '服务器地址',
     en: 'Host',
+    fr: 'Hôte',
   },
   brokerPort: {
     zh: '端口',
     en: 'Port',
+    fr: 'Port',
   },
   certType: {
     zh: '证书类型',
     en: 'Certificate',
+    fr: 'Certificat',
   },
   name: {
     zh: '名称',
     en: 'Name',
+    fr: 'Nom',
   },
   username: {
     zh: '用户名',
     en: 'Username',
+    fr: 'Nom d\'utilisateur',
   },
   password: {
     zh: '密码',
     en: 'Password',
+    fr: 'Mot de passe',
   },
   ca: {
     zh: 'CA 文件',
     en: 'CA File',
+    fr: 'Fichier CA',
   },
   cert: {
     zh: '客户端证书',
     en: 'Client Certificate File',
+    fr: 'Fichier de certificat client',
   },
   key: {
     zh: '客户端 key 文件',
     en: 'Client key file',
+    fr: 'Fichier de clé client',
   },
   connectionTimeout: {
     zh: '连接超时时长',
     en: 'Connect Timeout',
+    fr: 'Délai de connection dépassé',
   },
   cleanSession: {
     zh: '清除会话',
     en: 'Clean Session',
+    fr: 'Nettoyer la session',
   },
   autoReconnect: {
     zh: '自动重连',
     en: 'Auto Reconnect',
+    fr: 'Auto reconnexion',
   },
   mqttVersion: {
     zh: 'MQTT 版本',
     en: 'MQTT Version',
+    fr: 'Version de MQTT',
   },
   sessionExpiryInterval: {
     zh: '会话过期时间',
     en: 'Session Expiry Interval',
+    fr: 'Intervalle d\'expiration de session',
   },
   receiveMaximum: {
     zh: '接收最大数值',
     en: 'Receive Maximum',
+    fr: 'Réception maximum',
   },
   topicReuired: {
     zh: '请输入 Topic',
     en: 'Topic is required',
+    fr: 'Le Topic est requis',
   },
   payloadReuired: {
     zh: '请输入 Payload',
     en: 'Payload is required',
+    fr: 'Le Payload est requis',
   },
   color: {
     zh: '标记',
     en: 'Color',
+    fr: 'Couleur',
   },
   willMessage: {
     zh: '遗嘱消息',
     en: 'Last Will and Testament',
+    fr: 'Dernière volonté et testament',
   },
 }
