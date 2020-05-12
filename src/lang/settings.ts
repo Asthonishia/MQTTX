3eede61beca3635268f2ca58857c3b9fe18ef158
Asthonishia <korx.klesk@gmail.com>
2020-05-12 10:34:56 +0200

    French translation

--- src/lang/settings.ts
+++ src/lang/settings.ts
@@ -2,41 +2,51 @@ export default {
   settings: {
     zh: '设置',
     en: 'Settings',
+    fr: 'Paramètres',
   },
   general: {
     zh: '基础',
     en: 'General',
+    fr: 'Général',
   },
   language: {
     zh: '语言',
     en: 'Language',
+    fr: 'Langue',
   },
   automatically: {
     zh: '自动检查更新',
     en: 'Auto check update',
+    fr: 'Mise à jour automatique',
   },
   maxReconnectTimes: {
     zh: '最大重连次数',
     en: 'Max reconnection times',
+    fr: 'Temps de reconnexion max',
   },
   appearance: {
     zh: '外观',
     en: 'Appearance',
+    fr: 'Apparence',
   },
   theme: {
     zh: '主题',
     en: 'Theme',
+    fr: 'Thème',
   },
   light: {
     zh: '浅色',
     en: 'Light',
+    fr: 'Clair',
   },
   dark: {
     zh: '深色',
     en: 'Dark',
+    fr: 'Sombre',
   },
   advanced: {
     zh: '高级',
     en: 'Advanced',
+    fr: 'Avancé',
   },
 }
