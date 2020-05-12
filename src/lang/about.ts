83d700a2d5d60f868c2eb599f6b961a2bf0255e4
Asthonishia <korx.klesk@gmail.com>
2020-05-12 10:54:49 +0200

    French translation

--- src/lang/about.ts
+++ src/lang/about.ts
@@ -2,31 +2,38 @@ export default {
   about: {
     zh: '关于',
     en: 'About',
+    fr: 'À propos',
   },
   update: {
     zh: '检查更新',
     en: 'Check for Updates',
+    fr: 'Vérifier les mises à jour',
   },
   web: {
     zh: '官方网站',
     en: 'Website',
+    fr: 'Site internet',
   },
   support: {
     zh: '支持',
     en: 'Support',
+    fr: 'Support',
   },
   releases: {
     zh: '更新日志',
     en: 'Releases',
+    fr: 'Publications',
   },
   followGithub: {
     zh: '在 GitHub 上关注我们',
     en: 'Follow us on GitHub',
+    fr: 'Nous suivre sur GitHub',
   },
   emqxDesc: {
     zh: '一款完全开源，高可用低时延的百万级分布式物联网 MQTT 5.0 消息服务器。',
     // tslint:disable-next-line:max-line-length
     en: 'is a fully open source, highly scalable, highly available distributed MQTT 5.0 messaging broker for IoT, M2M and mobile applications.',
+    fr: 'est un courtier de messagerie MQTT 5.0 entièrement open source, hautement évolutif et hautement disponible pour les applications IoT, M2M et mobiles.',
   },
   emqxLocal: {
     zh: {
@@ -37,9 +44,14 @@ export default {
       emqx: 'EMQ X',
       local: 'To run MQTT Broker locally, {0} is recommended. ',
     },
+    fr: {
+      emqx: 'EMQ X',
+      local: 'Pour exécuter le courtier MQTT localement, {0} est recommandé. ',
+    }
   },
   emqxDocker: {
     zh: '使用 Docker 安装 EMQ X：',
     en: 'Install EMQ X by using Docker:',
+    fr: 'Installer EMQ X en utilisant Docker:',
   },
 }
