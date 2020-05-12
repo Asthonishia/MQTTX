1f287fba54e555842502b8830c57840169ac8e7a
Asthonishia <korx.klesk@gmail.com>
2020-05-12 10:50:41 +0200

    French tanslation

--- src/lang/common.ts
+++ src/lang/common.ts
@@ -2,85 +2,106 @@ export default {
   cancel: {
     zh: '取消',
     en: 'Cancel',
+    fr: 'Annuler',
   },
   confirm: {
     zh: '确定',
     en: 'Confirm',
+    fr: 'Confirmer',
   },
   inputRequired: {
     zh: '请输入',
     en: 'Please input',
+    fr: 'Champ requis',
   },
   selectRequired: {
     zh: '请选择',
     en: 'Please select',
+    fr: 'Sélection requise',
   },
   back: {
     zh: '返回',
     en: 'Back',
+    fr: 'Retour',
   },
   save: {
     zh: '保存',
     en: 'Save',
+    fr: 'Sauvegarde',
   },
   noData: {
     zh: '暂无数据',
     en: 'No Data',
+    fr: 'Aucune donnée',
   },
   createSuccess: {
     zh: '创建成功',
     en: 'Create Success',
+    fr: 'Création confirmée',
   },
   createfailed: {
     zh: '创建失败',
     en: 'Create Failed',
+    fr: 'Éched de la création',
   },
   editSuccess: {
     zh: '编辑成功',
     en: 'Edit Success',
+    fr: 'Édition confirmée',
   },
   editfailed: {
     zh: '编辑失败',
     en: 'Edit Failed',
+    fr: 'Échec de l\'édition',
   },
   deleteSuccess: {
     zh: '删除成功',
     en: 'Delete Success',
+    fr: 'Suppression confirmée',
   },
   deletefailed: {
     zh: '删除失败',
     en: 'Delete Failed',
+    fr: 'Échec de la suppression',
   },
   warning: {
     zh: '提示',
     en: 'Warning',
+    fr: 'Avertissement',
   },
   confirmDelete: {
     zh: '此操作将删除 {name}，是否继续？',
     en: 'This will delete {name}, continue?',
+    fr: 'Celà va supprimer {name}, continuer ?',
   },
   new: {
     zh: '新 建',
     en: 'New',
+    fr: 'Nouveau',
   },
   delete: {
     zh: '删 除',
     en: 'Delete',
+    fr: 'Supprimer',
   },
   edit: {
     zh: '编 辑',
     en: 'Edit',
+    fr: 'Éditer',
   },
   unitS: {
     zh: '秒',
     en: 's',
+    fr: 's',
   },
   unitMS: {
     zh: '毫秒',
     en: 'ms',
+    fr: 'ms',
   },
   config: {
     zh: '高级配置',
     en: 'Advanced Config',
+    fr: 'Paramètres avancés',
   },
 }
